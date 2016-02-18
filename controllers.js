bc504d79c5ad960d692cb5cda4757084a835d984
ankitgyawali <agyaw792@live.kutztown.edu>
2016-02-18 10:04:34 -0500

    scaffold with angular app

---
+++ controllers.js
@@ -0,0 +1,9 @@
+//Main login controller that handles login of three different user types
+angular.module('vrApp').controller('rootController', ['$scope', '$location', 
+    function($scope, $location) {
+
+ 
+console.log('TEST FOR ROOT CONTROLLER ');
+
+    }
+]);
--- main.js
+++ main.js
@@ -1,4 +1,24 @@
- /**
-     * You must include the dependency on 'ngMaterial' 
-     */
-    angular.module('BlankApp', ['ngMaterial']);
+// Designed by: Ankit Gyawali
+// Email: agyaw792@gmail.com
+// Description: Main App for VR creator
+
+//Define angular app and its dependencies
+var vrApp = angular.module('vrApp', ['ngMaterial','ngRoute']);
+
+// //Route configuration to provide different level of access controls to different user group
+// vrApp.config(function($routeProvider) {
+//     $routeProvider
+//         .when('/', {
+//             templateUrl: 'index.html',
+//             controller: 'rootController'
+//         })
+// });
+
+
+//Contains global variables used for checksheet manager
+//Authentication. Checks if user is logged in on every route change
+vrApp.run(['$rootScope',
+    function($rootScope) {
+ $rootScope.vrAppTitle = "Google Virtual Reality Creator"
+    	    }
+]);
